d6fb7ed689950a6e656d7ebe0507e33d4234fee7
amit8710 <83849830+amit8710@users.noreply.github.com>
2026-06-17 06:46:40 +0545

    Added Day 1 Python tasks and practice exercises

---
+++ day1.py
@@ -0,0 +1,71 @@
+"""
+#What is programming language?
+
+> A programming language is a formal language or a set of instructions that is used to communicate with the computer and machines to perform actions.
+> Compiled language: C, C++, Java ,The whole file is compiled once by the compiler.
+> Interpreted language: Python, JavaScript, Ruby, The code is executed line by line.
+
+> (HLL) High-Level Language: Python, Java, C#, It is easier to read and write, and it abstracts away the details of the computer's hardware.
+> (MLL) Mid-Level Language: C, C++, It provides a balance between high-level and low-level programming, allowing for more control over hardware while still being relatively easy to read and write.
+
+# Comments => Comments are written by using # for single line and for multi-line comments we can use ''' or  .(Multilne comments in python are not actually comments , but they are string.)
+
+# Why python is used in data science?
+> it can handle large data sets,also handle multiple IT sector efficiently.
+
+# Data Type
+1. init => whole numbers
+2. float => decimal numbers 
+3. string => sequence of characters wrapped in single or double quotes.
+
+# What is variable?
+> Variable are the programmer defined area to store the data in the memory.
+1. Never use number at the beginning of the variable name.
+2. Never use space in the variable name.
+3. Use underscore to separate words in the variable name.
+4. Use meaningful variable name.
+
+# Naming convention => Rules for writng names.
+1. PascalCase => The first letter of each word is capitalized. (e.g., MyVariableName)
+2. camelCase => The first letter of the first word is lowercase, and the first letter of each subsequent word is capitalized. (e.g., myVariableName).
+> These cases are mostly used ot name the class and function.
+
+# snake_case => All letters are lowercase, and words are separated by underscores. (e.g., my_variable_name).
+# Kebab-case => All letters are lowercase, and words are separated by hyphens. (e.g., my-variable-name).
+> This case is mostly used to name the variable and function.
+
+
+"""
+
+ # My first python code
+# name = "Amit Basnet"
+# print("Hello, Python it's me " + name)
+
+#Input() => It is used to take input from the user.
+# # To convert from one data type to another datatype , we use Type Casting.
+# num1 = input("Enter number 1: ")
+# num2 = input("Enter number 2: ")
+# result = int(num1) + int(num2)
+
+# print(result)
+
+#It shows the personal information that is obtain from the user.
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# favorite_team = input("Which is your favorite football team for the 2026 World Cup:  ")
+
+# print("Hello", name ,"!!")
+# print("Your age is", age, "years old")
+# print("Your favorite football team is", favorite_team)
+
+# Calculate the area of a Rectangle
+
+# length = input("Enter the length of the rectangle: ")
+# breadth = input("Enter the breadth of the rectangle: ") 
+
+# area = float(length) * float(breadth)
+
+# print("The area of the rectangle is: ", area)
+
+
